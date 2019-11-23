e698db50169282634af788eac4ac69b982f8e3ec
Mehmet Ali Arslan <mehmet-ali.arslan@klarna.com>
2019-11-23 14:03:25 +0100

    Types and circe for Json parsing

--- build.sbt
+++ build.sbt
@@ -1,6 +1,21 @@
+import sbt.addCompilerPlugin
+
 name := "ChanCOPter"
 
 version := "0.1"
 
-scalaVersion := "2.13.1"
+scalaVersion := "2.12.10"
 
+addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.1" cross CrossVersion.full)
+
+
+val circeVersion = "0.12.3"
+
+libraryDependencies ++= Seq(
+  "io.circe" %% "circe-core",
+  "io.circe" %% "circe-generic",
+  "io.circe" %% "circe-parser"
+).map(_ % circeVersion)
+
+libraryDependencies += "io.circe" %% "circe-generic-extras" %"0.12.2"
+libraryDependencies += "org.typelevel" %% "cats-effect" % "2.0.0"
---
+++ src/main/scala/types/types.scala
@@ -0,0 +1,66 @@
+package types
+
+import io.circe.{Decoder, Encoder}
+import io.circe.generic.JsonCodec
+import io.circe.generic.extras.semiauto.{deriveUnwrappedDecoder, deriveUnwrappedEncoder}
+
+@JsonCodec
+case class Market(products: List[Product])
+
+@JsonCodec
+case class Product(id: ProductId,
+                   name: ProductName,
+                   price: Price,
+                   channelIds: List[ChannelId])
+object Product {
+  implicit val ordering: Ordering[Product] = (a, b) =>
+    (a.channelIds.length / a.price.value * 1.0) compare (b.channelIds.length / b.price.value * 1.0)
+}
+
+@JsonCodec(encodeOnly = true)
+case class ProductSummary private (id: ProductId, name: ProductName, price: Price)
+object ProductSummary{
+  def apply(p: Product): ProductSummary = ProductSummary(p.id, p.name, p.price)
+}
+
+case class ProductId(value: String) extends AnyVal
+object ProductId {
+  implicit val decoder: Decoder[ProductId] = deriveUnwrappedDecoder
+  implicit val encoder: Encoder[ProductId] = deriveUnwrappedEncoder
+}
+
+case class ProductName(value: String) extends AnyVal
+object ProductName {
+  implicit val decoder: Decoder[ProductName] = deriveUnwrappedDecoder
+  implicit val encoder: Encoder[ProductName] = deriveUnwrappedEncoder
+}
+
+case class Price(value: Int) extends AnyVal
+object Price {
+  implicit val decoder: Decoder[Price] = deriveUnwrappedDecoder
+  implicit val encoder: Encoder[Price] = deriveUnwrappedEncoder
+}
+
+case class ChannelId(value: Int) extends AnyVal
+object ChannelId {
+  implicit val decoder: Decoder[ChannelId] = deriveUnwrappedDecoder
+  implicit val encoder: Encoder[ChannelId] = deriveUnwrappedEncoder
+}
+
+@JsonCodec(encodeOnly = true)
+case class Summary(totalChannels: Int,
+                   totalUniqueChannels: Int,
+                   totalPrice: Int,
+                   uniqueChannelIds: List[ChannelId],
+                   productsToBuy: List[ProductSummary])
+
+object Summary {
+  def apply(productsToBuy: List[Product]): Summary = {
+    val totalCh = productsToBuy.length
+    val uniqueChannels = productsToBuy.flatMap(_.channelIds).distinct
+    val totalPrice = productsToBuy.map(_.price.value).sum
+
+    Summary(totalCh, uniqueChannels.length, totalPrice, uniqueChannels, productsToBuy.map(ProductSummary(_)))
+
+  }
+}
